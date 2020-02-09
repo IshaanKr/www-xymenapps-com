84ee217ebb9f6262d782a817c65c258ae15bd4a1
Ishaan <i.k.ishaan786@gmail.com>
2020-02-10 00:26:13 +0530

    Moved links to JS

---
+++ js/my-javascript.js
@@ -0,0 +1,6 @@
+function attachClickEvent(){
+  document.getElementById("main-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-pro-latest-apk-download-url","_self");});
+  document.getElementById("xymen-pro-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-pro-latest-apk-download-url","_self");});
+  document.getElementById("xymen-lite-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-lite-latest-apk-download-url","_self");});
+}
+window.onload = attachClickEvent;
