d6b2fa6be2b1b96d2fdc81f0072c775fe8b38d50
Ishaan <i.k.ishaan786@gmail.com>
2020-02-10 00:54:24 +0530

    Added more links to JS

--- js/my-javascript.js
+++ js/my-javascript.js
@@ -2,5 +2,10 @@ function attachClickEvent(){
   document.getElementById("main-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-pro-latest-apk-download-url","_self");});
   document.getElementById("xymen-pro-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-pro-latest-apk-download-url","_self");});
   document.getElementById("xymen-lite-download-button").addEventListener("click", function(){window.open("https://link.xymenapps.com/xymen-lite-latest-apk-download-url","_self");});
+  document.getElementById("download-android-app").addEventListener("click", function(){window.open("https://link.xymenapps.com/download-play-store","_blank");});
+  document.getElementById("social-facebook").addEventListener("click", function(){window.open("https://www.facebook.com/XymenInc","_blank");});
+  document.getElementById("social-instagram").addEventListener("click", function(){window.open("https://www.instagram.com/xymen.apps/","_blank");});
+  document.getElementById("social-twitter").addEventListener("click", function(){window.open("https://twitter.com/XymenInc","_blank");});
+  document.getElementById("social-youtube").addEventListener("click", function(){window.open("https://www.youtube.com/channel/UC7P6XRqhId-9uDgnZ33UX3Q","_blank");});
 }
 window.onload = attachClickEvent;
